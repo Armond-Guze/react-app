377fee603707d31f739be4a913dd8f7770ff4817
Armond-Guze <Armond.Guze@yahoo.com>
2024-03-14 23:35:32 -0400

    created navbar

---
+++ react-portfolio/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Armond Guze
+      </a>
+      <ul>
+        <li><a href="/about-me">About Me</a></li>
+        <li><a href="/about-me">Resume</a></li>
+        <li><a href="/about-me">Contact Page</a></li>
+        <li><a href="/about-me">Github Account</a></li>
+      </ul>
+    </nav>
+  );
+}
